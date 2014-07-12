4f910552036cdc2d35fb791777184a1fd085f317
Marco Godínez <markotom@gmail.com>
2014-07-12 03:49:46 -0500

    Added new release grunt task

--- Gruntfile.js
+++ Gruntfile.js
@@ -3,6 +3,14 @@
 module.exports = function (grunt) {
 
   grunt.initConfig({
+    release: {
+      npm: false
+      push: false,
+      commit: false,
+      pushTags: false,
+      tagMessage: 'Version <%= version %>',
+      commitMessage: 'Release <%= version %>'
+    },
     wp_readme_to_markdown: {
       columnify: {
         files: {
@@ -13,6 +21,7 @@ module.exports = function (grunt) {
   });
 
   grunt.loadNpmTasks('grunt-wp-readme-to-markdown');
+  grunt.loadNpmTasks('grunt-release');
 
   // Register readme task in short
   grunt.registerTask('readme', 'wp_readme_to_markdown');
