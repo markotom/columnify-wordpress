0fcf5445e152f11c1dcf4be3613a93750dc6bfc3
Marco Godínez <markotom@gmail.com>
2014-07-12 02:12:43 -0500

    Code more nifty and faster with oop

--- columnify.php
+++ columnify.php
@@ -8,46 +8,95 @@ Author URI: https://github.com/markotom
 Version: 0.0.0
 */
 
-function get_dividers_by_sidebar( $sidebar ) {
-  $sidebars = wp_get_sidebars_widgets();
-  if ( isset( $sidebars[ $sidebar ] ) ) {
-    return preg_grep( '/column_divider/', $sidebars[ $sidebar ] );
-  }
-};
+function column_renderer() {
+  global $column_renderer;
 
-function get_column_class( $sidebar, $grid = 12 ) {
-  $dividers = count( get_dividers_by_sidebar( $sidebar ) );
-  if ( $dividers ) {
-    $columns = $dividers + 1;
-    $columns = $columns > $grid ? $grid : $columns;
-    return 'col-sm-' . floor( ( $grid / $columns ) );
-  }
+  $column_renderer = new Column_Renderer();
 }
 
-function widget_params( $params ) {
-  if ( $params[0][ 'widget_name' ] === 'Column Divider' ) {
-    $class = get_column_class( $params[ 0 ][ 'id' ] );
-    echo '</div><div class="' . $class . '">';
+add_filter( 'widgets_init', 'column_renderer' );
+
+/**
+ * Column Renderer
+ * Filter sidebars widgets and render columns
+ */
+class Column_Renderer {
+
+  var $grid = 12;
+  var $sidebars;
+  var $sidebars_widgets;
+
+  function __construct() {
+    // Get sidebars widgets
+    add_filter( 'sidebars_widgets', array( &$this, 'get_sidebars_widgets' ) );
+
+    // Create first column
+    add_action( 'dynamic_sidebar_before', array( &$this, 'dynamic_sidebar_before' ) );
+
+    // Create break column by column classes
+    add_filter( 'dynamic_sidebar_params', array( &$this, 'create_break_column' ) );
+
+    // Close last column div
+    add_action( 'dynamic_sidebar_after', array( &$this, 'dynamic_sidebar_after' ) );
   }
 
-  return $params;
+  public function get_sidebars_widgets( $sidebars_widgets ) {
+    $this->sidebars_widgets = $sidebars_widgets;
+    unset( $this->sidebars_widgets[ 'wp_inactive_widgets' ] );
+
+    foreach ( $this->sidebars_widgets as $sidebar => $widgets ) {
+      $current_sidebar = &$this->sidebars[ $sidebar ];
+      $current_sidebar[ 'dividers' ] = preg_grep( '/column_divider/', $widgets );
+      $current_sidebar[ 'column_classes' ] = $this->set_column_classes( $current_sidebar[ 'dividers' ] );
+    }
+
+    return $sidebars_widgets;
+  }
+
+  private function set_column_classes( $column_dividers ) {
+    $count_column_dividers = count( $column_dividers );
+
+    if ( $count_column_dividers > 0 ) {
+      $columns = $count_column_dividers + 1;
+      $columns = $columns > $this->grid ? $this->grid : $columns;
+
+      return 'col-sm-' . floor( ( $this->grid / $columns ) );
+    }
+  }
+
+  private function get_column_classes( $sidebar ) {
+    return $this->sidebars[ $sidebar ][ 'column_classes' ];
+  }
+
+  public function create_break_column( $params ) {
+    $widget_name = $params[ 0 ][ 'widget_name' ];
+    $widget_sidebar = $params[ 0 ][ 'id' ];
+
+    if ( ! is_admin () && $widget_name === 'Column Divider' ) {
+      $classes = $this->get_column_classes( $widget_sidebar );
+      echo '</div><div class="' . $classes . '">';
+    }
+
+    return $params;
+  }
+
+  public function dynamic_sidebar_before( $sidebar ) {
+    $classes = $this->get_column_classes( $sidebar );
+
+    if ( $classes ) {
+      echo '<div class="' . $classes . '">';
+    }
+  }
+
+  public function dynamic_sidebar_after( $sidebar ) {
+    $classes = $this->sidebars[ $sidebar ][ 'column_classes' ];
+
+    if ( $classes ) {
+      echo '</div>';
+    }
+  }
+
 }
-
-add_filter( 'dynamic_sidebar_params', 'widget_params' );
-
-add_action( 'dynamic_sidebar_before', function ($sidebar) {
-  $class = get_column_class( $sidebar );
-  if ( $class ) {
-    echo '<div class="' . $class . '">';
-  }
-});
-
-add_action( 'dynamic_sidebar_after', function ($sidebar) {
-  $class = get_column_class( $sidebar );
-  if ( $class ) {
-    echo '</div>';
-  }
-});
 
 function register_widgets() {
   // Register Column_Divider_Widget
@@ -58,7 +107,7 @@ add_action( 'widgets_init', 'register_widgets' );
 add_action( 'widgets_admin_page', array( 'Column_Divider_Widget', 'headers' ), -1000 );
 
 /**
- * Column Divider
+ * Column Divider Widget
  * Separate your widgets by columns
  */
 class Column_Divider_Widget extends WP_Widget {
@@ -89,7 +138,5 @@ class Column_Divider_Widget extends WP_Widget {
   }
 
 }
-
-
 
 ?>
